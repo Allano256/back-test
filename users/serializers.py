3cbbd95a2d015ee8f527208388e212dd638941bd
allan zizinga <allanzizinga@yahoo.com>
2024-10-19 00:48:02 +0200

    Created djoser serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,1 +1,10 @@
-from
+from django.contrib.auth import get_user_model
+from djoser.serializers import UserCreateSerializer
+
+
+User = get_user_model()
+
+class CreateUserSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = User
+        fields = ['id','email','first_name', 'last_name', 'password']
