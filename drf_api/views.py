5ce805aa287e38b285d0dc208447e9af4f267c95
Allan Zizinga <allanzizinga@yahoo.com>
2024-10-23 19:59:14 +0000

    Fixed typo error on api_view

--- drf_api/views.py
+++ drf_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view
+@api_view()
 def root_route(request):
     return Response ({
         'message': 'Welcome to the Remember API.'
