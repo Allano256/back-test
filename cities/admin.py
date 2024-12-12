185151f96ad627f4ff0404769997db49e7a4b856
Allan Zizinga <allanzizinga@yahoo.com>
2024-12-12 23:27:11 +0000

    Added max_length to notes

--- cities/admin.py
+++ cities/admin.py
@@ -2,6 +2,5 @@
 from django.contrib import admin
 from .models import NewCity
 
-# Register your models here.
 
 admin.site.register(NewCity)
--- cities/models.py
+++ cities/models.py
@@ -3,18 +3,14 @@ from django.conf import settings
 
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class NewCity(models.Model):
     user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     city_name=models.CharField(max_length=150)
     date=models.DateField(auto_now_add=True)
-    notes= models.TextField()
+    notes= models.TextField(max_length=200)
     lng=models.FloatField(default=0.0)
     lat=models.FloatField(default=0.0)
 
     def __str__(self):
         return f'{self.city_name}, {self.date}, {self.notes}'
-
-
- 
