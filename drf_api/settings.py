8f359ef77c34c58e6b0c2c9ac73dac9e96ebb0b0
allan zizinga <allanzizinga@yahoo.com>
2025-01-12 21:16:19 +0100

    Changed link of image on READMe

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-allano256-backtest-2arkx3886yl.ws.codeinstitute-ide.net",
